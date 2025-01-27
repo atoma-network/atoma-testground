618138f3d3183021f2f1fd04dea2902e34dcfe78
chad <chad.nehemiah94@gmail.com>
2025-01-27 12:56:26 -0500

    build: add base template for running local instances

---
+++ interop/terraform/modules/short_lived/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+resource "aws_key_pair" "atoma" {
+  key_name_prefix = "atoma-"
+  public_key      = file("${path.module}/files/atoma.pub")
+}
+
+resource "aws_instance" "atoma" {
+  tags = {
+    Name = "atoma-node"
+  }
+
+  launch_template {
+    name = "atoma-node"
+  }
+
+  key_name = aws_key_pair.atoma.key_name
+}
+
+output "public_ip" {
+  value = aws_instance.atoma.public_ip
+}
