31045efcc7c7390f19a191ad0d311245bdd88e0b
chad <chad.nehemiah94@gmail.com>
2025-05-05 22:37:51 -0500

    build: updated sql scripts

--- interop/scripts/node_setup.sh
+++ interop/scripts/node_setup.sh
@@ -33,7 +33,6 @@ cd /opt/atoma
 
 # Clone your repository with the docker-compose files
 # Use the branch specified in the environment variable, defaulting to main if not set
-# BRANCH=${NODE_BRANCH:-main}
-BRANCH=mc/feat/integrate-sglang
+BRANCH=${NODE_BRANCH:-main}
 git clone -b $BRANCH https://github.com/atoma-network/atoma-node.git .
 
